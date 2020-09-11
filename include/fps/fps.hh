36b94d974c9de1c4dbd0c73334faff0223b94131
Jason Kimko <22348009+jskimko@users.noreply.github.com>
2020-09-11 15:18:35 -0700

    Update fps return codes.

--- include/fps/fps.hh
+++ include/fps/fps.hh
@@ -3,10 +3,10 @@
 
 namespace fps {
 
-static constexpr int SUCCESS       = 0;
-static constexpr int ERROR_GENERIC = 1;
-static constexpr int ERROR_USAGE   = 2;
-static constexpr int ERROR_FFMPEG  = 3;
+static constexpr int SUCCESS = 0;
+static constexpr int ERROR_GENERIC = 101;
+static constexpr int ERROR_USAGE   = 102;
+static constexpr int ERROR_FFMPEG  = 103;
 
 void foo();
 
